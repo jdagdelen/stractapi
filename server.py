ebd6fd60f82c0b08f7e52048196e20b917f71f1f
jdagdelen <jdagdelen@lbl.gov>
2018-07-31 12:28:26 -0700

    reqparsing version.

--- server.py
+++ server.py
@@ -1,20 +1,107 @@
-from flask import Flask
-from flask_restful import Api
-from flask_marshmallow import Marshmallow
-from stract.api.resources import *
+from flask import Flask, request, jsonify
+from stract.api.models import *
+from matstract.models.database import AtlasConnection
+from matstract.models.word_embeddings import EmbeddingEngine
+from matstract.models.search import Search
+
+db = AtlasConnection()
+ee = EmbeddingEngine()
 
 app = Flask(__name__)
-api = Api(app)
-ma = Marshmallow(app)
 
-api.add_resource(Abstracts, '/abstracts/<string:abstract_id>', endpoint='abstracts_endpoint')
-api.add_resource(Search, '/api/search/')
-api.add_resource(Embeddings, '/api/embeddings/<string:wordphrase>', endpoint='embeddings_endpoint')
-api.add_resource(CompoundEmbeddings, '/api/embeddings/compound/<string:wordphrase>',
-                       endpoint='compound_embeddings_endpoint')
-api.add_resource(ClusterPlot, '/api/plots/cluster/')
-api.add_resource(TrendsPlot, '/api/plots/trends/')
-api.add_resource(Test, '/api/test/<string:message>')
+
+# endpoint to test
+@app.route('/api/test/<message>', methods=["Get"])
+def test_api(message):
+    messages = message.split(',')
+    test = [APITest(message) for message in messages]
+    return TestSchema(many=True).jsonify(test)
+
+
+# endpoint to abstracts
+@app.route('/api/abstracts/<abstract_id>', methods=["POST"])
+def retrieve_abstracts(abstract_id):
+    abstract_ids = abstract_id.split(',')
+    entries = db.get_documents_by_id(abstract_ids)
+    abstracts = [Abstract(**entry, id=str(entry["_id"])) for entry in entries]
+    return AbstractSchema(many=True).jsonify(abstracts)
+
+
+# endpoint to search
+@app.route('/api/search/', methods=['Post'])
+def execute_search():
+    filter = SearchFilter(**request.json)
+    pass
+
+
+# endpoint to more like this searches
+@app.route('/api/morelikethis/', methods=['Post'])
+def more_like_this():
+    filter = SearchFilter(**request.json)
+    conditions = {
+        "text": filter.text,
+        "material": {"positive": filter.material},
+        "property": {"positive": filter.property},
+        "application": {"positive": filter.application},
+        "characterization": {"positive": filter.characterization}
+        # TODO: figure out a better way of doing this
+    }
+    search = Search.from_dict(conditions)
+    return jsonify(search.results)
+
+
+# endpoint to similar materials
+@app.route('/api/materials/similar/<material>', methods=['Get'])
+def get_similar_materials(material):
+    pass
+    # return jsonify(GET_SIMILAR_MATERIALS(material, cutoff)
+
+
+# endpoint to materials summary
+@app.route('/api/materials/<material>', methods=['Get'])
+def get_material_summary(material):
+    pass
+
+
+# TODO: Create DB of materials and metadata
+
+
+# endpoint to embeddings
+@app.route('/api/mat2vec/embeddings/<wordphrase>', methods=["Get"])
+def retrieve_embeddings(wordphrase):
+    wps = wordphrase.split(',')
+    embeddings = []
+    for wp in wps:
+        embeddings.append(Embedding(wp, '', ee.get_word_vector(wp), compound=True))
+    return EmbeddingSchema(many=True).jsonify(embeddings)
+
+
+## TODO: Fix compound vs non-compound embeddings
+
+
+# endpoint to synonyms
+@app.route('/api/mat2vec/synonyms/<wordphrase>', methods=['Get'])
+def get_synonyms(wordphrase):
+    return jsonify(ee.close_words(wordphrase, top_k=10)[0])
+
+
+# endpoint to cluster plots
+@app.route('/api/plots/cluster/', methods=['Get'])
+def get_cluster_plot(config):
+    return NotImplementedError()
+
+
+# endpoint to cluster plots
+@app.route('/api/plots/trends/', methods=['Get'])
+def get_trends_plot(config):
+    return NotImplementedError()
+
+
+# # endpoint to synonyms
+# @app.route('/api/mat2vec/synonyms/<wordphrase>', methods=['Get'])
+# def get_synonyms(wordphrase):
+#     print(ee.close_words(wordphrase,  top_k=10))
+#     return jsonify(ee.close_words(wordphrase,  top_k=10)[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- stract/api/models.py
+++ stract/api/models.py
@@ -1,8 +1,12 @@
-from server import ma
-from marshmallow.fields import String, Integer, List, Nested
+import marshmallow.fields as fields
+from flask_marshmallow import Marshmallow
 from flask_marshmallow.fields import URLFor
-from marshmallow import post_dump
+import json
+import numpy as np
 
+# Field options are String, Integer, List, Number, Nested, Url, etc
+
+ma = Marshmallow()
 
 class Abstract:
     """
@@ -11,7 +15,20 @@ class Abstract:
 
     """
 
-    def __init__(self, id, title, authors, date, abstract, doi, journal, keywords, link, source):
+    id = fields.String(required=True)
+    # uri = URLFor('abstracts_endpoint')
+    title = fields.String()
+    authors = fields.List(fields.String())
+    year = fields.String()
+    abstract = fields.String()
+    doi = fields.String()
+    journal = fields.String()
+    keywords = fields.List(fields.String())
+    link = fields.String()
+    source = fields.String(default='scopus')
+
+
+    def __init__(self, id, title, authors, year, abstract, doi, journal, keywords, link, source='scopus', **kwargs):
         """
         Create an Abstract.
 
@@ -31,7 +48,7 @@ class Abstract:
         self.title = title
         self.authors = authors
         self.abstract = abstract
-        self.date = date
+        self.date = year
         self.journal = journal
         self.keywords = keywords
         self.doi = doi
@@ -41,7 +58,12 @@ class Abstract:
 
 class AbstractSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'title', 'authors', 'year', 'abstract', 'doi', 'journal', 'keywords', 'link', 'source')
+        # fields to expose
+        fields = ('id', 'title', 'authors', 'abstract', 'date', 'journal', 'keywords', 'doi', 'source', 'link')
+
+
+abstract_shema = AbstractSchema()
+abstracts_shema = AbstractSchema(many=True)
 
 
 class Material:
@@ -51,8 +73,22 @@ class Material:
 
     """
 
-    def __init__(self, formula, mpid_exists, mpid, chemical_system, properties, characterizations, phases,
+    uri = URLFor('materials_endpoint')
+    name = fields.String(default="NOT IMPLEMENTED")
+    formula = fields.String()
+    mpid_exists = fields.Boolean()
+    mpid = fields.String()
+    chemical_system = fields.List(fields.String())
+    properties = fields.List(fields.String())
+    characterizations = fields.List(fields.String())
+    phases = fields.List(fields.String())
+    descriptors = fields.List(fields.String())
+    applications = fields.List(fields.String())
+    synthesis = fields.List(fields.String())
+
+    def __init__(self, name, formula, mpid_exists, mpid, chemical_system, properties, characterizations, phases,
                  applications, descriptors, synthesis):
+        self.name = name
         self.formula = formula
         self.mpid_exists = mpid_exists
         self.mpid = mpid
@@ -67,14 +103,33 @@ class Material:
 
 class MaterialSchema(ma.Schema):
     class Meta:
-        fields = ('formula', 'mpid_exists', 'mpid', 'chemical_system', 'properties',
+        # Fields to expose
+        fields = ('name', 'formula', 'mpid_exists', 'mpid', 'chemical_system', 'properties',
                       'characterizations', 'phases', 'applications', 'descriptors', 'synthesis')
 
+
+# TODO: Add in models for all entities so we can do more interesting stuff with them.
 
 class SearchFilter:
     """
     Schema for specifying how to filter and sort search results.
     """
+    text = fields.String()
+    author = fields.List(fields.String())
+    year = fields.List(fields.String())
+    journal = fields.List(fields.String())
+    keywords = fields.List(fields.String())
+    material = fields.List(fields.String())
+    formula = fields.List(fields.String())
+    chemsys = fields.List(fields.String())
+    property = fields.List(fields.String())
+    characterization = fields.List(fields.String())
+    phase = fields.List(fields.String())
+    descriptor = fields.List(fields.String())
+    application = fields.List(fields.String())
+    synthesis = fields.List(fields.String())
+    sortby = fields.String(default='relevance')
+    limit = fields.Integer(default=1000)
 
     def __init__(self, text, author, year, journal, keywords, material, properties, characterizations, phases,
                  applications, descriptors, synthesis):
@@ -94,6 +149,7 @@ class SearchFilter:
 
 class SearchFilterSchema(ma.Schema):
     class Meta:
+        # Fields to expose
         fields = ('text', 'author', 'year', 'journal', 'keywords', 'material', 'properties',
                       'characterizations', 'phases', 'applications', 'descriptors', 'synthesis')
 
@@ -102,17 +158,30 @@ class Embedding:
     """
     A Mat2Vec word/phrase embedding. Embeddings are 300-dimensional vectors that represent the meaning
     of words and phrases in a mathematical way. Matstract embeddings are derived from the Word2Vec
-    NLP architecture (Google, 2013).
+    NLP architecture (Google, 2013). If no embedding exists for the single wordphrase, the sum of the embeddings
+    of the sub-wordphrases is used.
     """
 
-    def __init__(self, wordphrase, tag, embedding):
+    uri = URLFor('embeddings_endpoint')
+    wordphrase = fields.String()
+    tag = fields.String()
+    embedding = fields.List(fields.Decimal())
+    compound = fields.Boolean(default=False)
+
+    def __init__(self, wordphrase, tag, embedding, compound):
         self.wordphrase = wordphrase
         self.tag = tag
-        self.embedding = embedding
+        # ndarrays aren't json serializable, for now we'll just cast to a list.
+        if isinstance(embedding, np.ndarray):
+            self.embedding = embedding.tolist()
+        else:
+            self.embedding = embedding
+        self.compound = compound
 
 class EmbeddingSchema(ma.Schema):
     class Meta:
-        fields = ('wordphrase', 'tag', 'embedding')
+        # Fields to expose
+        fields = ('wordphrase', 'tag', 'embedding', 'compound')
 
 
 class ClusterPlot:
@@ -123,17 +192,23 @@ class ClusterPlot:
     are not meaningful. Note The graph is stored as a string representation of the dict of Plotly parameters.
     """
 
+    plot = fields.Dict()
+
     def __init__(self, plot):
         self.plot = plot
 
 class ClusterPlotSchema(ma.Schema):
-    class Meta:
-        fields = ('plot')
+    plot = fields.Dict()
 
 class ClusterPlotConfig:
     """
     Parameters for constructing cluster plots.
     """
+
+    heatphrase = fields.String()
+    entity_type = fields.String(default='all')
+    wordphrases = fields.List(fields.String())
+    limit = fields.Integer()
 
     def __init__(self, heatphrase, entity_type, wordphrases, limit):
         self.heatphrase = heatphrase
@@ -143,6 +218,7 @@ class ClusterPlotConfig:
 
 class ClusterPlotConfigSchema(ma.Schema):
     class Meta:
+        # Fields to expose
         fields = ('heatphrase', 'entity_type', 'wordphrases', 'limit')
 
 
@@ -153,17 +229,23 @@ class TrendsPlot:
     dict of Plotly parameters.
     """
 
+    plot = fields.Dict()
+
     def __init__(self, plot):
         self.plot = plot
 
 class TrendsPlotSchema(ma.Schema):
-    class Meta:
-        fields = ('plot')
+
+    plot = fields.Dict()
 
 class TrendsPlotConfig:
     """
     Parameters for constructing trends plots.
     """
+
+    filter = fields.Nested(SearchFilterSchema())
+    begin = fields.String()
+    end = fields.String()
 
     def __init__(self, filter, begin, end):
         self.filter = filter
@@ -172,6 +254,7 @@ class TrendsPlotConfig:
 
 class TrendsPlotConfigSchema(ma.Schema):
     class Meta:
+        # Fields to expose
         fields = ('filter', 'begin', 'end')
 
 class APITest:
@@ -179,9 +262,10 @@ class APITest:
     Helps with testing the API
     """
 
+    message = fields.String()
+
     def __init__(self, message):
         self.message = message
 
 class TestSchema(ma.Schema):
-    class Meta:
-        fields = ('message')
+    message = fields.String()
